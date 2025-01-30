ddbdc3c20e8fcf7aadb68fb8301ba1fcf08750cd
batuhankiskac <kiskacbatuhan@gmail.com>
2025-01-30 12:02:36 +0300

    Add header file for philosopher project with necessary includes and guards

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 11:57:46 by bkiskac           #+#    #+#             */
+/*   Updated: 2025/01/30 12:00:47 by bkiskac          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
