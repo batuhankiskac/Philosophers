2bb0d83835bc42155ec80aaded775837f07e0aa8
Batuhan Kıskaç <bkiskac@k1m33s05.42istanbul.com.tr>
2025-03-20 12:39:21 +0300

    added

--- philo/parse_input.c
+++ philo/parse_input.c
@@ -6,13 +6,30 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 19:27:42 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/03/16 10:13:08 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/03/18 13:25:08 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static long	ft_atol(const char *str)
+static int	is_valid_number(char *str)
+{
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '+' || *str == '-')
+		str++;
+	if (!(*str >= '0' && *str <= '9'))
+		return (0);
+	while (*str)
+	{
+		if (!(*str >= '0' && *str <= '9'))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+static long	ft_atol(char *str)
 {
 	long	result;
 	int		sign;
@@ -35,10 +52,15 @@ static long	ft_atol(const char *str)
 	return (result * sign);
 }
 
-static long	check_range(const char *str)
+static long	check_range(char *str)
 {
 	long	num;
 
+	if (!is_valid_number(str))
+	{
+		printf("Invalid Input!\n");
+		return (ERROR);
+	}
 	num = ft_atol(str);
 	if (num < 1 || num > 2147483647)
 		return (ERROR);
@@ -48,12 +70,16 @@ static long	check_range(const char *str)
 int	parse_input(int argc, char *argv[], t_program *prog)
 {
 	prog->philo_num = (int)check_range(argv[1]);
+	if (prog->philo_num < 1)
+		return (ERROR);
 	prog->time_to_die = (size_t)check_range(argv[2]);
+	if (prog->time_to_die == (size_t)ERROR)
+		return (ERROR);
 	prog->time_to_eat = (size_t)check_range(argv[3]);
+	if (prog->time_to_eat == (size_t)ERROR)
+		return (ERROR);
 	prog->time_to_sleep = (size_t)check_range(argv[4]);
-	if (prog->philo_num > 200 || prog->philo_num < 1
-		|| prog->time_to_die < 60 || prog->time_to_eat < 60
-		|| prog->time_to_sleep < 60)
+	if (prog->time_to_sleep == (size_t)ERROR)
 		return (ERROR);
 	if (argc == 6)
 	{
